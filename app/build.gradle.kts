1397fe16c87003fa257b42d05725c5da50a20773
Vanita Rathod <vanita.rathod@inkeysolutions.com>
2023-10-07 15:25:31 +0530

    sdsd

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,14 +33,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-
-    androidComponents {
-        beforeVariants { variantBuilder ->
-            // To check for a certain build type, use variantBuilder.buildType == "<buildType>"
-            if (variantBuilder.productFlavors.containsAll(listOf("api" to "minApi21", "mode" to "demo"))) {
-                // Gradle ignores any variants that satisfy the conditions above.
-                variantBuilder.enable = false
-            }
+    kotlin {
+        jvmToolchain {
+            (this as JavaToolchainSpec).languageVersion.set(JavaLanguageVersion.of(17))
         }
     }
 }
